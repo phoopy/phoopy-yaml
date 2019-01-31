9322ab6a999fd9eac5e8d33a5e06a24ade742271
Jeferson Daniel <jeferson.daniel412@gmail.com>
2019-01-31 12:46:32 -0200

    Fixing test

--- test/test_yaml_parser.py
+++ test/test_yaml_parser.py
@@ -22,24 +22,20 @@ class TestYamlParser(object):
             google:
                 client_id: ~
                 client_secret: ~
-        overwrite: ~
+        a: ~
+        b: {}
         '''
         services = '''
         imports:
             - { resource: 'parameters.yml' }
-        overwrite: {}
+        services:
+            lala:
+                class: Lala
+        a: {}
+        b: ~
         '''
         fs.create_file('parameters.yml', contents=parameters)
         fs.create_file('services.yml', contents=services)
-        actual_result = yaml_parser.parse('services.yml')
-        expected_result = {
-            'parameters': {
-                'env': 'test',
-                'google': {
-                    'client_id': None,
-                    'client_secret': None
-                }
-            },
-            'overwrite': {}
-        }
-        assert expected_result == actual_result
+        result = yaml_parser.parse('services.yml')
+        assert 'test' == result['parameters']['env']
+        assert 'Lala' == result['services']['lala']['class']
